545aef70a16b3ff3de02dddccdd5b55abcbf460f
sud-Z <61097308+sud-Z@users.noreply.github.com>
2020-02-24 13:40:11 -0500

    Update assign_update.php

--- assign_update.php
+++ assign_update.php
@@ -17,8 +17,8 @@
 		$eventError = null;
 		
 		// keep track post values
-	    $customerId = $_POST['customerId'];
-		$eventId = $_POST['eventId'];
+	    $customerId = $_POST['customer_Id'];
+		$eventId = $_POST['event_Id'];
 		
 		// validate input
 		$valid = true;
@@ -38,7 +38,7 @@
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql = "UPDATE assignments  set assign_per_id = ?, assign_event_id = ? WHERE id = ?";
 			$q = $pdo->prepare($sql);
-			$q->execute(array($customerId,$eventId));
+			$q->execute(array($customerId,$eventId,$id));
 			Database::disconnect();
 			header("Location: assignments.php");
 		}
@@ -72,93 +72,65 @@
 		    			<h3>Update an Assignment</h3>
 		    		</div>
 		    		
-		    		 <div class="row">
-                        <h4>Customers</h4>
-                    </div>
-		    		
-		    			<table class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Name</th>
-                          <th>Email Address</th>
-                          <th>Mobile Number</th>
-                          <th>ID</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                       
-                       $pdo = Database::connect();
-                       $sql = 'SELECT * FROM customers ORDER BY id DESC';
-                       foreach ($pdo->query($sql) as $row) {
-                                echo '<tr>';
-                                echo '<td>'. $row['name'] . '</td>';
-                                echo '<td>'. $row['email'] . '</td>';
-                                echo '<td>'. $row['mobile'] . '</td>';
-                                echo '<td>'. $row['id'] . '</td>';
-                                echo '</tr>';
-                       }
-                       Database::disconnect();
-                      ?>
-                      </tbody>
-                </table>
-                
-                 <div class="row">
-                        <h4>Events</h4>
-                    </div>
-                    
-                <table class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Date</th>
-                          <th>Time</th>
-                          <th>Location</th>
-                          <th>Descritpion</th>
-                          <th>ID</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                       $pdo = Database::connect();
-                       $sql = 'SELECT * FROM events ORDER BY id DESC';
-                       foreach ($pdo->query($sql) as $row) {
-                                echo '<tr>';
-                                echo '<td>'. $row['event_date'] . '</td>';
-                                echo '<td>'. $row['event_time'] . '</td>';
-                                echo '<td>'. $row['event_location'] . '</td>';
-                                echo '<td>'. $row['event_description'] . '</td>';
-                                echo '<td>'. $row['id'] . '</td>';
-                                echo '</tr>';
-                       }
-                       Database::disconnect();
-                      ?>
-                      </tbody>
-                </table>
+		    <form class="form-horizontal" action="assign_update.php?id=<?php echo $id?>" method="post">
+	
+		
+				<div class="control-group">
+					<label class="control-label">Customer</label>
+					<div class="controls">
+						<?php
+							$pdo = Database::connect();
+							$sql = 'SELECT * FROM customers ORDER BY name ASC';
+							echo "<select class='form-control' name='customer_Id' id='customer_Id'>";
+							
+							foreach ($pdo->query($sql) as $row) {
+								if($row['id']==$customerId)
+									echo "<option selected value='" . $row['id'] . " '> " . $row['name'] . ', ' .$row['mobile'] . "</option>";
+								else
+									echo "<option value='" . $row['id'] . " '> " . $row['name'] . ', ' .$row['mobile'] . "</option>";
+							}
+							echo "</select>";
+							Database::disconnect();
+						?>
+					</div>	<!-- end div: class="controls" -->
+				</div> <!-- end div class="control-group" -->
+				
+				<div class="control-group">
+					<label class="control-label">Event</label>
+					<div class="controls">
+						<?php
+							$pdo = Database::connect();
+							$sql = 'SELECT * FROM events ORDER BY event_date ASC, event_time ASC';
+							echo "<select class='form-control' name='event_Id' id='event_Id'>";
+							foreach ($pdo->query($sql) as $row) {
+								if($row['id']==$event) {
+									echo "<option selected value='" . $row['id'] . " '> " . $row['event_date'] . " (" . $row['event_time'] . ") - " .
+									trim($row['event_description']) . " (" . 
+									trim($row['event_location']) . ") " .
+									"</option>";
+								}
+								else {
+									echo "<option value='" . $row['id'] . " '> " . $row['event_date'] . " (" . $row['event_time'] . ") - " .
+									trim($row['event_description']) . " (" . 
+									trim($row['event_location']) . ") " .
+									"</option>";
+								}
+							}
+							echo "</select>";
+							Database::disconnect();
+						?>
+					</div>	<!-- end div: class="controls" -->
+				</div> <!-- end div class="control-group" -->
+			  
+				
+				<div class="form-actions">
+					<button type="submit" class="btn btn-success">Confirm</button>
+						<a class="btn" href="assignments.php">Back</a>
+				</div>
+				
+			</form>
     		
-	    			<form class="form-horizontal" action="assign_update.php?id=<?php echo $id?>" method="post">
-					  <div class="control-group <?php echo !empty($customerError)?'error':'';?>">
-					    <label class="control-label">Customer ID</label>
-					    <div class="controls">
-					      	<input name="customerId" type="text"  placeholder="Customer ID" value="<?php echo !empty($customerId)?$customerId:'';?>">
-					      	<?php if (!empty($customerError)): ?>
-					      		<span class="help-inline"><?php echo $customerError;?></span>
-					      	<?php endif; ?>
-					    </div>
-					  </div>
-					  <div class="control-group <?php echo !empty($eventError)?'error':'';?>">
-					    <label class="control-label">Event ID</label>
-					    <div class="controls">
-					      	<input name="eventId" type="text" placeholder="Event ID" value="<?php echo !empty($eventId)?$eventId:'';?>">
-					      	<?php if (!empty($eventError)): ?>
-					      		<span class="help-inline"><?php echo $eventError;?></span>
-					      	<?php endif;?>
-					    </div>
-					  </div>
-					  <div class="form-actions">
-						  <button type="submit" class="btn btn-success">Update</button>
-						  <a class="btn" href="assignments.php">Back</a>
-						</div>
-					</form>
+	    		
 				</div>
 				
     </div> <!-- /container -->
